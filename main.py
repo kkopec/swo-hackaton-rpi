6e4a95d3e641e952da80c827c761d988fc618e44
kkopec <kopeckrzysztoff@gmail.com>
2020-02-28 14:17:01 +0100

    button

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import json
 import time
 
 STATUS_URL = 'https://hackaton.azurewebsites.net/status'
-PINS = [25, 8, 7, 11]
+ACTION_URL = 'https://hackaton.azurewebsites.net/action'
+PINS_OUT = [25, 8, 7, 11]
+PIN_IN = 24
 
-STATE = dict([(25,0), (8,0), (7,0)])
+STATE = dict([(pin, 0) for pin in PINS_OUT])
 
 def get_status():
     r = requests.get(STATUS_URL)
@@ -30,7 +32,7 @@ def show_status(status):
 
     if status == "Failure" and STATE[25] == 0:
             GPIO.output(11, GPIO.LOW)
-            time.sleep(0.5)
+            time.sleep(0.25)
             GPIO.output(11, GPIO.HIGH)
 
     [set_state(k,0) if k != pin else set_state(k, 1) for k,v in STATE.items()]
@@ -39,23 +41,35 @@ def show_status(status):
 def set_state(pin, value):
     STATE[pin] = value
 
-def sigterm_handler(a1, a2):
-    GPIO.cleanup(PINS)
+def lamp_sigterm_handler(a1, a2):
+    GPIO.cleanup(PINS_OUT)
     os.exit(0)
+
+def button_sigterm_handler(a1, a2):
+    GPIO.remove_event_detect(PIN_IN)
+    GPIO.cleanup(PIN_IN)
+    os.exit(0)
+
+def on_button_pressed():
+    print('Button pressed!')
+    r = requests.post(ACTION_URL, data= {})
+
+    if r.status_code != 200:
+        print(f'NOPE! -  ${r.status_code}')
 
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
     #GPIO.setup(PINS, GPIO.OUT, initial=GPIO.HIGH)
 
-    child_pid = None
+    lamp_pid, button_pid = None, None
     try:
-        child_pid = os.fork()
-        if child_pid == 0:
-            signal.signal(signal.SIGTERM, sigterm_handler)
-            #GPIO.setmode(GPIO.BCM)
-            GPIO.setup(PINS, GPIO.OUT, initial=GPIO.HIGH)
+        lamp_pid = os.fork()
+        if lamp_pid == 0:
+            # lamp child
+            signal.signal(signal.SIGTERM, lamp_sigterm_handler)
+            GPIO.setup(PINS_OUT, GPIO.OUT, initial=GPIO.HIGH)
             GPIO.output(11, GPIO.LOW)
-            time.sleep(0.5)
+            time.sleep(0.1)
             GPIO.output(11, GPIO.HIGH)
             try:
                 while True:
@@ -63,11 +77,30 @@ if __name__ == "__main__":
                     show_status(status)
                     time.sleep(10)
             except KeyboardInterrupt:
-                sigterm_handler()
+                lamp_sigterm_handler()
         else:
-            pid, status = os.waitpid(child_pid, 0)
+            button_pid = os.fork()
+            if button_pid == 0:
+                # button child
+                signal.signal(signal.SIGTERM, button_sigterm_handler)
+                GPIO.setup(PIN_IN, GPIO.IN)
+
+                try:
+                    GPIO.add_event_detect(PIN_IN, GPIO.RISING, callback=on_button_pressed, bouncetime=10000)
+                    while True:
+                        sleep(1)
+
+                except KeyboardInterrupt:
+                    button_sigterm_handler()
+
+
+            # parent
+            for child in [lamp_pid, button_pid]:
+                os.waitpid(child, 0)
+
     except KeyboardInterrupt:
-        os.kill(child_pid, signal.SIGTERM)
+        os.kill(lamp_pid, signal.SIGTERM)
+        os.kill(button_pid, signal.SIGTERM)
         #GPIO.cleanup(PINS)
 
     #GPIO.cleanup(PINS)
